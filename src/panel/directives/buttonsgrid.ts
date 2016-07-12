5bdcff5d52e1bae98eb859fd425e429594b28829
Антон Домолазов <anton@domelaz.ru>
2016-07-12 21:49:15 +0300

    Move link function into directive

--- src/panel/directives/buttonsgrid.ts
+++ src/panel/directives/buttonsgrid.ts
@@ -18,56 +18,54 @@ const render = (values: number[], selected: number[]) => {
   return `<div class="${css.wrapperClass}">${wrapped}</div>`;
 };
 
-const link = (scope, element, attrs) => {
-  scope.$watch(attrs.ngModel, (next: IMaterials, prev: IMaterials) => {
-    if (next) {
-      const data = render(next.width, scope.widths);
-      element.html(data);
-    }
-  }, true);
-
-  const toggle = (event) => {
-    const target = jQuery(event.target);
-
-    if (target.hasClass(css.wrapperClass)) {
-      // Hit between the buttons
-      return;
-    }
-
-    const value = parseFloat(target.text());
-
-    if (isNaN(value)) {
-      return;
-    }
-
-    let widths = typeof(scope.widths) === "string"
-      ? scope.widths.split(/[ ,]/).map(parseFloat)
-      : scope.widths.slice(0);
-
-    if (widths.indexOf(value) === -1) {
-      target.addClass(css.activeWidthClass);
-      widths.push(value);
-    } else {
-      if (widths.length > 1) {
-        target.removeClass(css.activeWidthClass);
-        widths.splice(scope.widths.indexOf(value), 1);
-      }
-    };
-    scope.$apply(() => {
-      scope.setAppData({ "widths": widths.sort() });
-    });
-  };
-
-  element.on("click", toggle);
-
-  element.on("$destroy", () => {
-    element.off();
-  });
-};
-
 const directive = () => {
   return {
-    link,
+    link: (scope, element, attrs) => {
+      scope.$watch(attrs.ngModel, (next: IMaterials, prev: IMaterials) => {
+        if (next) {
+          const data = render(next.width, scope.widths);
+          element.html(data);
+        }
+      }, true);
+
+      const toggle = (event) => {
+        const target = jQuery(event.target);
+
+        if (target.hasClass(css.wrapperClass)) {
+          // Hit between the buttons
+          return;
+        }
+
+        const value = parseFloat(target.text());
+
+        if (isNaN(value)) {
+          return;
+        }
+
+        let widths = typeof(scope.widths) === "string"
+          ? scope.widths.split(/[ ,]/).map(parseFloat)
+          : scope.widths.slice(0);
+
+        if (widths.indexOf(value) === -1) {
+          target.addClass(css.activeWidthClass);
+          widths.push(value);
+        } else {
+          if (widths.length > 1) {
+            target.removeClass(css.activeWidthClass);
+            widths.splice(scope.widths.indexOf(value), 1);
+          }
+        };
+        scope.$apply(() => {
+          scope.setAppData({ "widths": widths.sort() });
+        });
+      };
+
+      element.on("click", toggle);
+
+      element.on("$destroy", () => {
+        element.off();
+      });
+    },
     require: "ngModel",
     restrict: "E",
   };
