2b41c14ca6e705e0f8fa1565e3a4f3980758f4b7
Антон Домолазов <anton@domelaz.ru>
2016-07-12 19:38:47 +0300

    Stop solver with action

--- src/panel/services/solver.ts
+++ src/panel/services/solver.ts
@@ -15,12 +15,17 @@ const service = (
    */
   let deferred: ng.IDeferred<ISolution>;
 
+  let stopInProgress = false;
+
   /**
    * Send solution to upstream
    */
   const notify = (solution: ISolution) => {
     redux.dispatch(pushSolution(solution));
     deferred.notify(solution);
+    if (stopInProgress) {
+      throw new Error("Stop recieved");
+    }
   };
 
   /**
@@ -31,6 +36,7 @@ const service = (
   const start = (data, options): ng.IPromise<ISolution> => {
     deferred = $q.defer();
     const fn = notify.bind(this, solution1);
+    stopInProgress = false;
 
     $timeout( fn, Math.random() * 10000 ).then(() => {
       return $timeout( fn, Math.random() * 10000 );
@@ -53,6 +59,8 @@ const service = (
     }).then(() => {
       // Ctrl+V foreva, но промис нужно разрезолвить.
       deferred.resolve();
+    }).finally(() => {
+      console.info("Cleanup and exit here");
     });
 
     return deferred.promise;
@@ -63,6 +71,16 @@ const service = (
       deferred.reject(reason);
     }
   };
+
+  const staySync = () => {
+    const state = <IFlowState>redux.getState().flow;
+    if (deferred && !state.solverBusy) {
+      stopInProgress = true;
+      return stop();
+    }
+  };
+
+  redux.subscribe(staySync);
 
   return {
     start,
