16d01ca00e590da8c7a0be7d9946db216d4e31d4
Антон Домолазов <anton@domelaz.ru>
2016-07-15 14:09:41 +0300

    Report interfaces

--- src/interfaces.d.ts
+++ src/interfaces.d.ts
@@ -305,3 +305,42 @@ interface ISolution {
    */
   dimensions: number[];
 }
+
+interface IReportDataIn {
+  /**
+   * Площадь Области Размещения, pt
+   */
+  area: number;
+
+  /**
+   * Площадь контура, pt
+   */
+  contour: number;
+
+  /**
+   * Количество контуров в ОР, шт.
+   */
+  contours: number;
+
+  /**
+   * Вал, pt
+   */
+  height: number;
+
+  /**
+   * Тираж, шт.
+   */
+  printing: number;
+}
+
+interface IReportDataOut {
+  /**
+   * Процент облоя
+   */
+  obloy: number;
+
+  /**
+   * Количество погонных метров материала
+   */
+  material: number;
+}
